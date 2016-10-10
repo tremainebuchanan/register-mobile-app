433d16c73dad4f5370ed6c2672a60e2b3b5b9397
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2016-10-10 18:09:48 -0500

    Quick fixes

--- app/src/main/java/com/tremainebuchanan/register/activities/Main.java
+++ app/src/main/java/com/tremainebuchanan/register/activities/Main.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -57,6 +60,27 @@ public class Main extends AppCompatActivity {
         }else{
             Toast.makeText(this, "No connection", Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private class GetRegistersTask extends AsyncTask<String, Void, ArrayList<Session>>{
